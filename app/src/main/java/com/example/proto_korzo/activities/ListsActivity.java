10ed8e37d7f739fb2f3eb3c9d34d556518e7612a
AlexStibbons <pantic.aleks@gmail.com>
2019-09-25 16:35:20 +0200

    another starter commit

--- app/src/main/java/com/example/proto_korzo/activities/ListsActivity.java
+++ app/src/main/java/com/example/proto_korzo/activities/ListsActivity.java
@@ -4,13 +4,17 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.proto_korzo.R;
 
 public class ListsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.act_lists);
+       setContentView(R.layout.act_list);
     }
+
+    // get all/some/a limited number of films & list them up
+    // add a star to corner of card - persist fave/unfave
+    // on list click (AdapterView / OnItemClickListener) open new activity
 }
--- app/src/main/java/com/example/proto_korzo/activities/MainActivity.java
+++ app/src/main/java/com/example/proto_korzo/activities/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.proto_korzo.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.proto_korzo.R;
 
@@ -10,7 +14,40 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // create scaffold
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_main);
+
+        // populate layout? or initialize necessary variables?
+        Button enter = (Button) findViewById(R.id.btn_enter);
+        EditText email = (EditText) findViewById(R.id.login_email);
+        EditText password = (EditText) findViewById(R.id.login_password);
+
+        // why won't it accept a lambda expression here?
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Main/login activity", "clicking?");
+
+                // if both email and pass are not null
+                if (hasData("email", "pass")) { // will a static work like this?
+                    // persist data
+                    // pass user id to list activity
+                    // enter list activity
+                    Log.d("Main/login activity", "data checked; true");
+                } else {
+                    // error
+                    Log.e("Main/login activity", "error during hasData check");
+                }
+
+            }
+        });
+
+    }
+    private static boolean hasData(String email, String pass) {
+        if (email != null && pass !=null) {
+            return true;
+        }
+            return false;
     }
 }
